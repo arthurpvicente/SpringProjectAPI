1549aba5a235e59aa81e8cf0b39a45a918ea9656
arthurpvicente <arthur.p.v@hotmail.com>
2024-07-30 16:01:14 -0300

    first commit

---
+++ src/main/java/com/springboot/SpringProjectApplication.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
